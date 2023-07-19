0c95b58a5e4a9e56934774bb49771bb9d4d87b37
lucadipisa <lucaldp@outlook.com>
2023-07-19 22:08:04 +0200

    add icons btn & header image

--- lib/main.dart
+++ lib/main.dart
@@ -10,65 +10,67 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'UFC Stats', // Titre de l'application
+      title: 'UFC STATS', // Titre de l'application
       theme: ThemeData(
         primarySwatch: Colors.red, // Modifier la couleur en rouge
       ),
-<<<<<<< HEAD
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('UFC Stats'), // Titre de l'AppBar
+          title: const Text('UFC STATS'), // Titre de l'AppBar
           centerTitle: true, // Centrer le titre de l'AppBar
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                'assets/images/logo.png',
-                width: 150,
-                height: 150,
-              ), // Charger l'image depuis le dossier local
-              const SizedBox(height: 5), // Espacement entre l'image et le titre
-              const Text(
-                "Les meilleures stats de l'UFC :",
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Image.asset(
+              'assets/images/Ciryl-Gane-UFC.png', // Chemin de l'image
+              fit: BoxFit.cover, // Ajuster la taille de l'image pour couvrir toute la largeur
+            ),
+            const SizedBox(height: 120), // Espacement entre l'image et le reste en bas
+            Image.asset(
+              'assets/images/logo.png',
+              width: 100,
+              height: 100,
+            ), // Charger l'image depuis le dossier local
+            const SizedBox(height: 32), // Espacement entre l'image et le titre
+            const Center(
+              child: Text(
+                "Les meilleures stats de l'UFC",
                 style: TextStyle(
                   fontSize: 24,
                   fontWeight: FontWeight.bold,
                   color: Color.fromARGB(255, 0, 0, 0), // Modifier la couleur en rouge
                 ),
               ),
-              const SizedBox(height: 32), // Espacement entre le titre et les boutons
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      // Naviguer vers l'écran des combattants
-                    }, // Bouton "Fighters"
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: Size(150, 50), // Taille minimale du bouton
-                    ),
-                    child: const Text('Fighters'),
+            ),
+            const SizedBox(height: 32), // Espacement entre le titre et les boutons
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton.icon(
+                  onPressed: () {
+                    // Naviguer vers l'écran des combattants
+                  },
+                  icon: const Icon(Icons.sports_mma), 
+                  label: const Text('Fighters'), // Texte du bouton "Fighters"
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(150, 50), // Taille minimale du bouton
                   ),
-                  const SizedBox(width: 16), // Espacement horizontal entre les boutons
-                  ElevatedButton(
-                    onPressed: () {
-                      // Naviguer vers l'écran des événements
-                    }, // Bouton "Evenements"
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: Size(150, 50), // Taille minimale du bouton
-                    ),
-                    child: const Text('Evenements'),
+                ),
+                const SizedBox(width: 16), // Espacement horizontal entre les boutons
+                ElevatedButton.icon(
+                  onPressed: () {
+                    // Naviguer vers l'écran des événements
+                  },
+                  icon: const Icon(Icons.calendar_today),
+                  label: const Text('Evenements'), // Texte du bouton "Evenements"
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(150, 50), // Taille minimale du bouton
                   ),
-                ],
-              ),
-            ],
-=======
-      home: const Text(
-          'Marc est un trisomique, raciste, misogine, obèse, etc...'
->>>>>>> 7f3b57876ceeb88193275f2ea0735b647822ee27
-          ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
