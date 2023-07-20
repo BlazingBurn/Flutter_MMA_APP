38937b9c0d102f9bdb30739db2289dd136a9533d
lucadipisa <lucaldp@outlook.com>
2023-07-20 11:11:33 +0200

    fighters cards

--- lib/fighters.dart
+++ lib/fighters.dart
@@ -2,13 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_mma_app/scaffoldGeneral.dart';
 
 class Fighters extends StatelessWidget {
-
   const Fighters({super.key});
 
   @override
   Widget build(BuildContext context) {
     return ScaffoldGeneral(redirection: "/", widget: _fighters());
-
   }
 
   Widget _fighters() {
@@ -16,5 +14,4 @@ class Fighters extends StatelessWidget {
             child: Text("Fighters"),
           );
   }
-
 }
