3c65de74e028eff5431dd95a14197ea83b6f2e8d
Hrex <cedgoujon78@gmail.com>
2023-07-20 09:47:26 +0200

    Modification page Fighter.dart

--- lib/fighter.dart
+++ lib/fighter.dart
@@ -1,29 +1,113 @@
 import 'package:flutter/material.dart';
 
 class Fighter extends StatelessWidget {
-
   const Fighter({super.key, required this.fighterId});
 
   final String fighterId;
 
   @override
   Widget build(BuildContext context) {
+    int crossAxisCount = MediaQuery.of(context).size.width < 600 ? 4 : 8;
     return MaterialApp(
-      title: 'UFC STATS',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-      ),
-      home: Scaffold(
+        title: 'UFC STATS',
+        theme: ThemeData(
+          primarySwatch: Colors.red,
+        ),
+        home: Scaffold(
           appBar: AppBar(
             title: const Text('UFC STATS'), // Titre de l'AppBar
             centerTitle: true, // Centrer le titre de l'AppBar
           ),
-          body: Center(
-            child:
-              Text("fighterId : $fighterId"),
-          )
+          body: SingleChildScrollView(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("fighterId : $fighterId"),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      width: 150,
+                      height: 150,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                          image: AssetImage('assets/images/mma-fight.jpg'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 20),
+                    const Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'NOM',
+                            style: TextStyle(fontSize: 18),
+                          ),
+                          Text(
+                            'PRENOM',
+                            style: TextStyle(fontSize: 18),
+                          ),
+                          Text(
+                            'UN SURNOM',
+                            style: TextStyle(
+                                fontSize: 16, fontStyle: FontStyle.italic),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  'Petit texte',
+                  style: TextStyle(fontSize: 16),
+                ),
+                const SizedBox(height: 10),
+                GridView.builder(
+                  shrinkWrap: true,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: crossAxisCount,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                  ),
+                  itemCount: 8,
+                  itemBuilder: (context, index) {
+                    return ProgressBlock(
+                      title: 'Titre ${index + 1}',
+                      progress:
+                          0.5, // Remplacez cette valeur par la progression réelle
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+}
+
+class ProgressBlock extends StatelessWidget {
+  String title;
+  double progress;
+
+  ProgressBlock({required this.title, required this.progress});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(title),
+          const SizedBox(height: 10),
+          LinearProgressIndicator(value: progress),
+        ],
       ),
     );
   }
-
 }
